1d01ca31a0e7fe1dc32dbcd55e5f696916c276f4
sharasaran <sharaitqa@gmail.com>
2023-01-10 18:00:59 +0530

    average

--- src/Exercise2/Exer2.java
+++ src/Exercise2/Exer2.java
@@ -4,7 +4,23 @@ public class Exer2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//method for creating array
+		
+		int num[] = {1,2,3,4,5,6,7,8,9,10};
+		float total_num = 0;
+		float average_num = 0;
+		
+		
+		for(int i=0;i<num.length; i++) {
+		total_num =total_num + num[i];
+		
+		
+		}
+		System.out.println(total_num);
+	//average equation
+		average_num =total_num/num.length;
+		System.out.println(average_num);
 	}
 
 }
---
+++ src/Exercise_1/Exe_1.java
@@ -0,0 +1,15 @@
+package Exercise_1;
+
+public class Exe_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+     	int[]num = {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0; i<num.length ;i++)
+		{
+			System.out.print(num[i]);
+		}
+		
+	}
+}
