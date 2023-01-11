9627deb77d2fc0128ac0ddd3816701c3bcd98ced
Sharasaran <sharaitqa@gmail.com>
2023-01-11 14:39:53 +0530

    test
    exercise

--- src/Exercise_1/Question_8.java
+++ src/Exercise_1/Question_8.java
@@ -3,7 +3,7 @@ package Exercise_1;
 public class Question_8 {
 	public static void main (String[] arg ) {
 		// TODO Auto-generated method stub
-System.out.println("hii");
+System.out.println("hiiihghj");
 	}
 
 }
